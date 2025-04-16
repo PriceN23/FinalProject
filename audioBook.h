ff40d15103a5ec20460919d1a1a90c3059c6458e
PriceN23 <PriceN23@uww.edu>
2025-04-15 23:11:38 -0500

    Adding audioBook.h

---
+++ audioBook.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "data.h"
+
+class audio_book : public data {
+private:
+	bool rating; //tumb up/thumb down
+
+public:
+	audio_book(const std::string& title,
+		const std::string& creator,
+		int year, 
+		int duration,
+		bool rating);
+
+	bool get_rating() const;
+
+	void print() const override;
+};
